1d63082b1934830e90fe1bdaf79649846a64e7e3
CaioLyra71 <caiolyra1@hotmail.com>
2023-02-28 18:31:49 -0300

    Corrigido bug de números negativos no exércicio03.

--- exercício03.js
+++ exercício03.js
@@ -1,8 +1,8 @@
-const ladoA = 10
-const ladoB = 10
+const ladoA = 0
+const ladoB = 0
 
-if (ladoA > 6 || ladoB > 6) {
-    console.log("Entrada inválida, as peças do dominó só vão até 6 em cada lado")
+if ((ladoA > 6 || ladoA < 0) || (ladoB > 6 || ladoB < 0)) {
+    console.log("Entrada inválida, as peças do dominó vão de 1 até 6 em cada lado")
 } else if (ladoA === ladoB) {
     console.log("SIM!")
 } else {
