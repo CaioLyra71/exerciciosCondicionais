4c009e79778a6168d6b420ae71085d11347d389e
CaioLyra71 <caiolyra1@hotmail.com>
2023-02-28 17:06:45 -0300

    primeira resolução exercício1

---
+++ exercício01.js
@@ -0,0 +1,19 @@
+const jogada1 = "pedra"
+const jogada2 = "tesoura"
+
+if (jogada1 === jogada2) {
+    console.log("Empate!!!")
+} else if (jogada1 === "pedra" && jogada2 === "papel") {
+    console.log("papel!!")
+} else if (jogada1 === "pedra" && jogada2 === "tesoura") {
+    console.log("pedra!!")
+} else if (jogada1 === "papel" && jogada2 === "pedra") {
+    console.log("papel!!")
+} else if (jogada1 === "papel" && jogada2 === "tesoura") {
+    console.log("tesoura!!")
+} else if (jogada1 === "tesoura" && jogada2 === "pedra") {
+    console.log("pedra!!")
+} else if (jogada1 === "tesoura" && jogada2 === "papel") {
+    console.log("tesoura!!")
+}
+
