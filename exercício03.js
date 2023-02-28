fd11f8d6bf0a80a53ef4954e34a17a2901c8e244
CaioLyra71 <caiolyra1@hotmail.com>
2023-02-28 17:42:45 -0300

    Adicionado exclamação

--- exercício03.js
+++ exercício03.js
@@ -1,10 +1,10 @@
-const ladoA = 3
-const ladoB = 4
+const ladoA = 10
+const ladoB = 10
 
 if (ladoA > 6 || ladoB > 6) {
     console.log("Entrada inválida, as peças do dominó só vão até 6 em cada lado")
 } else if (ladoA === ladoB) {
     console.log("SIM!")
 } else {
-    console.log("NÃO")
+    console.log("NÃO!")
 }
