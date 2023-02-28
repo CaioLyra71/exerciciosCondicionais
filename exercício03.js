505088dc5ff0d16cd55d3d2db98ee0118e5c0d01
CaioLyra71 <caiolyra1@hotmail.com>
2023-02-28 19:00:32 -0300

    corrigida mensagem de erro

--- exercício03.js
+++ exercício03.js
@@ -2,7 +2,7 @@ const ladoA = 0
 const ladoB = 0
 
 if ((ladoA > 6 || ladoA < 0) || (ladoB > 6 || ladoB < 0)) {
-    console.log("Entrada inválida, as peças do dominó vão de 1 até 6 em cada lado")
+    console.log("Entrada inválida, as peças do dominó vão de 0 até 6 em cada lado")
 } else if (ladoA === ladoB) {
     console.log("SIM!")
 } else {
