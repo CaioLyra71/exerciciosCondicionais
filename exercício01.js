f1450d7a1a8184e0622cdb7c8b6f158f1080833f
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-01 09:56:51 -0300

    alterado retorno para através do conteúdo das variáveis

--- exercício01.js
+++ exercício01.js
@@ -1,19 +1,19 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "tesoura"
+const jogada2 = "pedra"
 
 if (jogada1 === jogada2) {
     console.log("Empate!!!")
 } else if (jogada1 === "pedra" && jogada2 === "papel") {
-    console.log("papel!!")
+    console.log(jogada2)
 } else if (jogada1 === "pedra" && jogada2 === "tesoura") {
-    console.log("pedra!!")
+    console.log(jogada1)
 } else if (jogada1 === "papel" && jogada2 === "pedra") {
-    console.log("papel!!")
+    console.log(jogada1)
 } else if (jogada1 === "papel" && jogada2 === "tesoura") {
-    console.log("tesoura!!")
+    console.log(jogada2)
 } else if (jogada1 === "tesoura" && jogada2 === "pedra") {
-    console.log("pedra!!")
+    console.log(jogada2)
 } else if (jogada1 === "tesoura" && jogada2 === "papel") {
-    console.log("tesoura!!")
+    console.log(jogada1)
 }
 
