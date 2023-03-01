c3f9222b6337085c2201ec38549cbe2ebaba0b17
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-01 19:57:36 -0300

    Exercício 15 atualizado

--- exercício15.js
+++ exercício15.js
@@ -2,21 +2,15 @@ const primeiroNome = "Caio"
 const sobrenome = ""
 const apelido = ""
 
-if (primeiroNome === "") {
+if (!primeiroNome) {
     console.log("Preencha o primeiro nome")
 } else {
-    if (primeiroNome && sobrenome && apelido) {
-        console.log(`${primeiroNome} ${sobrenome} - "${apelido}"`)
-    } else if (primeiroNome && sobrenome) {
-        console.log(primeiroNome, sobrenome)
-    } else if (primeiroNome && apelido) {
-        console.log(`${primeiroNome} - "${apelido}"`)
-    } else if (primeiroNome) {
-        console.log(primeiroNome)
-    } else if (apelido) {
+    if (apelido) {
         console.log(apelido)
     } else if (sobrenome) {
         console.log(sobrenome)
+    } else if (primeiroNome) {
+        console.log(primeiroNome)
     }
 }
 
