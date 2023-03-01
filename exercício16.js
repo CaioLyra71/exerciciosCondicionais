d476f855f1aba57b49978b246e39708233f0e1a7
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-01 20:08:55 -0300

    Exercício 16 finalizado

---
+++ exercício16.js
@@ -0,0 +1,8 @@
+const idadeDoAluno = 15
+const possuiResponsavel = false
+
+if (idadeDoAluno >= 18 || possuiResponsavel === true) {
+    console.log("Rematrícula realizada com sucesso")
+} else {
+    console.log("Não é possivel fazer a rematrícula")
+}
